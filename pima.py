b4c2cfa6d8accca113b3c6b0ac92dd66a2d5debe
Inlic <bwiedeback@gmail.com>
2021-01-28 16:24:16 -0700

    updated script to include standardization technique

--- pima.py
+++ pima.py
@@ -1,4 +1,5 @@
 from csv import reader
+from math import sqrt
 
 def load_csv(filename):
     dataset = list()
@@ -40,8 +41,35 @@ def normalize_dataset(dataset, minmax):
         for i in range(len(row)):
             row[i] = (row[i] - minmax[i][0]) / (minmax[i][1] - minmax[i][0])
 
-minmax = dataset_minmax(dataset)
-print(minmax)
+# minmax = dataset_minmax(dataset)
+# print(minmax)
+#
+# normalize_dataset(dataset,minmax)
+# print(dataset[0])
 
-normalize_dataset(dataset,minmax)
+def column_means(dataset):
+    means = [0 for i in range(len(dataset[0]))]
+    for i in range(len(dataset[0])):
+        col_values = [row[i] for row in dataset]
+        means[i] = sum(col_values) / float(len(dataset))
+    return means
+
+def column_stdevs(dataset, means):
+    stdevs = [0 for i in range(len(dataset[0]))]
+    for i in range(len(dataset[0])):
+        variance = [pow(row[i]-means[i],2) for row in dataset]
+        stdevs[i] = sum(variance)
+    stdevs = [sqrt(x/(float(len(dataset)-1))) for x in stdevs]
+    return stdevs
+
+def standardize_dataset(dataset, means, stdevs):
+    for row in dataset:
+        for i in range(len(row)):
+            row[i] = (row[i] - means[i]) /stdevs[i]
+
+means = column_means(dataset)
+stdevs = column_stdevs(dataset,means)
+print(means)
+print(stdevs)
+standardize_dataset(dataset,means,stdevs)
 print(dataset[0])
