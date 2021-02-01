0dd56caa1f1b8e33b6c25aed3c10c7691aac5ba3
Inlic <bwiedeback@gmail.com>
2021-02-01 10:16:06 -0700

    Testing out Evaluation Metrics

---
+++ EvaluationMetrics.py
@@ -0,0 +1,54 @@
+def accuracy_metric(actual,predicted):
+    correct = 0
+    for i in range(len(actual)):
+        if actual[i] == predicted[i]:
+            correct += 1
+    return correct / float(len(actual)) * 100
+
+actual = [0,0,0,0,0,1,1,1,1,1]
+predicted = [0,1,0,0,0,1,0,1,1,1]
+accuracy = accuracy_metric(actual,predicted)
+print(accuracy)
+print("#######################")
+
+def confusion_matrix(actual, predicted):
+    unique = set(actual)
+    matrix = [list() for x in range(len(unique))]
+    for i in range(len(unique)):
+        matrix[i] = [0 for x in range(len(unique))]
+    lookup = dict()
+    for i, value in enumerate(unique):
+        lookup[value] = i
+    for i in range(len(actual)):
+        x = lookup[actual[i]]
+        y = lookup[predicted[i]]
+        matrix[y][x] += 1
+    return unique, matrix
+
+def print_confusion_matrix(unique, matrix):
+    print('(A)'+' '.join(str(x) for x in unique))
+    print('(P)---')
+    for i, x in enumerate(unique):
+        print("%s| %s" % (x, ' '.join(str(x) for x in matrix[i])))
+
+
+actual = [0,0,0,0,0,1,1,1,1,1]
+predicted = [0,1,1,0,0,1,0,1,1,1]
+unique, matrix = confusion_matrix(actual, predicted)
+print_confusion_matrix(unique,matrix)
+print("#######################")
+
+# Interpretation of confusion matrix
+# Prediction errors on the left diagonal - One 1 counted as 0, Two 0 counted as 1
+
+def mae_metric(actual,predicted):
+    sum_error = 0.0
+    for i in range(len(actual)):
+        sum_error += abs(predicted[i]-actual[i])
+    return sum_error / float(len(actual))
+
+# Test out MAE
+actual = [0.1,0.2,0.3,0.4,0.5]
+predicted = [0.11, 0.19, 0.29, 0.41, 0.5]
+mae = mae_metric(actual, predicted)
+print(mae)
