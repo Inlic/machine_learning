828bc912418abc4ad7aaf2f9a75b026d75f97d19
Inlic <bwiedeback@gmail.com>
2021-02-03 15:55:13 -0700

    updated multvariate

--- MultivariateLinearRegression.py
+++ MultivariateLinearRegression.py
@@ -41,7 +41,7 @@ from random import randrange
 from csv import reader
 from math import sqrt
 
-def load_csv_file(filename):
+def load_csv(filename):
     dataset = list()
     with open(filename, 'r') as file:
         csv_reader = reader(file)
@@ -89,5 +89,59 @@ def rmse_metric(actual, predicted):
     mean_error = sum_error / float(len(actual))
     return sqrt(mean_error)
 
+def evaluate_algorithm(dataset, algorithm, n_folds, *args):
+    folds = cross_validation_split(dataset,n_folds)
+    scores = list()
+    for fold in folds:
+        train_set = list(folds)
+        train_set.remove(fold)
+        train_set = sum(train_set, [])
+        test_set = list()
+        for row in fold:
+            row_copy = list(row)
+            test_set.append(row_copy)
+            row_copy[-1] = None
+        predicted = algorithm(train_set, test_set, *args)
+        actual = [row[-1] for row in fold]
+        rmse = rmse_metric(actual, predicted)
+        scores.append(rmse)
+    return scores
 
+def predict(row,coefficients):
+    yhat = coefficients[0]
+    for i in range(len(row)-1):
+        yhat += coefficients[i+1]*row[i]
+    return yhat
 
+def coefficients_sgd(train, l_rate, n_epoch):
+    coef = [0.0 for i in range(len(train[0]))]
+    for i in range(n_epoch):
+        for row in train:
+            yhat = predict(row, coef)
+            error = yhat - row[-1]
+            coef[0] = coef[0] - l_rate * error
+            for i in range(len(row)-1):
+                coef[i+1] = coef[i+1] - l_rate * error * row[i]
+    return coef
+
+def linear_regression_sgd(train, test, l_rate, n_epoch):
+    predictions = list()
+    coef = coefficients_sgd(train, l_rate, n_epoch)
+    for row in test:
+        yhat = predict(row, coef)
+        predictions.append(yhat)
+    return(predictions)
+
+seed(1)
+filename = 'wineQualityWhite.csv'
+dataset = load_csv(filename)
+for i in range(len(dataset[0])):
+    str_column_to_float(dataset,i)
+minmax = dataset_minax(dataset)
+normalize_dataset(dataset,minmax)
+n_folds = 5
+l_rate = 0.01
+n_epoch = 100
+scores = evaluate_algorithm(dataset, linear_regression_sgd, n_folds, l_rate, n_epoch)
+print('Scores: %s' % scores)
+print('Mean RMSE: %.3f' % (sum(scores)/float(len(scores))))
