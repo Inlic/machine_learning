504e0a3c779475e1490db40168ad345abd1bc62b
Inlic <bwiedeback@gmail.com>
2021-02-03 10:00:34 -0700

    added print statements for clarity removed error checking

--- TestHarnesses.py
+++ TestHarnesses.py
@@ -1,77 +1,78 @@
 # Harness for algorithm evaluation
 
-# def evaluate_algorithm(dataset, algorithm, split, *args):
-#     train, test = train_test_split(dataset, split)
-#     test_set = list()
-#     for row in test:
-#         row_copy = list(row)
-#         row_copy[-1] = None
-#         test_set.append(row_copy)
-#     predicted = algorithm(train,test_set, *args)
-#     actual = [row[-1] for row in test]
-#     accuracy = accuracy_metric(actual, predicted)
-#     return accuracy
+def evaluate_algorithm(dataset, algorithm, split, *args):
+    train, test = train_test_split(dataset, split)
+    test_set = list()
+    for row in test:
+        row_copy = list(row)
+        row_copy[-1] = None
+        test_set.append(row_copy)
+    predicted = algorithm(train,test_set, *args)
+    actual = [row[-1] for row in test]
+    accuracy = accuracy_metric(actual, predicted)
+    return accuracy
 
 from random import seed
 from random import randrange
 from csv import reader
 
-# def load_csv(filename):
-#     dataset = list()
-#     with open(filename, 'r') as file:
-#         csv_reader = reader(file)
-#         for row in csv_reader:
-#             if not row:
-#                 continue
-#             dataset.append(row)
-#         return dataset
-#
-# def str_column_to_float(dataset,column):
-#     for row in dataset:
-#         row[column] = float(row[column].strip())
+def load_csv(filename):
+    dataset = list()
+    with open(filename, 'r') as file:
+        csv_reader = reader(file)
+        for row in csv_reader:
+            if not row:
+                continue
+            dataset.append(row)
+        return dataset
 
-# def train_test_split(dataset, split):
-#     train = list()
-#     train_size = split * len(dataset)
-#     dataset_copy = list(dataset)
-#     while len(train) < train_size:
-#         index = randrange(len(dataset_copy))
-#         train.append(dataset_copy.pop(index))
-#     return train, dataset_copy
+def str_column_to_float(dataset,column):
+    for row in dataset:
+        row[column] = float(row[column].strip())
 
-# def accuracy_metric(actual, predicted):
-#     correct = 0
-#     for i in range(len(actual)):
-#         if actual[i] == predicted[i]:
-#             correct += 1
-#     return correct / float(len(actual)) * 100.0
+def train_test_split(dataset, split):
+    train = list()
+    train_size = split * len(dataset)
+    dataset_copy = list(dataset)
+    while len(train) < train_size:
+        index = randrange(len(dataset_copy))
+        train.append(dataset_copy.pop(index))
+    return train, dataset_copy
 
-# def evaluate_algorithm(dataset, algorithm, split, *args):
-#     train, test = train_test_split(dataset, split)
-#     test_set = list()
-#     for row in test:
-#         row_copy = list(row)
-#         row_copy[-1] = None
-#         test_set.append(row_copy)
-#     predicted = algorithm(train, test_set, *args)
-#     actual = [row[-1] for row in test]
-#     accuracy = accuracy_metric(actual, predicted)
-#     return accuracy
+def accuracy_metric(actual, predicted):
+    correct = 0
+    for i in range(len(actual)):
+        if actual[i] == predicted[i]:
+            correct += 1
+    return correct / float(len(actual)) * 100.0
 
-# def zero_rule_algorithm_classification(train, test):
-#     output_values = [row[-1] for row in train]
-#     prediction = max(set(output_values), key=output_values.count)
-#     predicted = [prediction for i in range(len(test))]
-#     return predicted
+def evaluate_algorithm(dataset, algorithm, split, *args):
+    train, test = train_test_split(dataset, split)
+    test_set = list()
+    for row in test:
+        row_copy = list(row)
+        row_copy[-1] = None
+        test_set.append(row_copy)
+    predicted = algorithm(train, test_set, *args)
+    actual = [row[-1] for row in test]
+    accuracy = accuracy_metric(actual, predicted)
+    return accuracy
 
-# seed(1)
-# filename = 'PimaData.csv'
-# dataset = load_csv(filename)
-# for i in range(len(dataset[0])):
-#     str_column_to_float(dataset, i)
-# split = 0.6
-# accuracy = evaluate_algorithm(dataset, zero_rule_algorithm_classification, split)
-# print('Accuracy: %.3f%%' % (accuracy))
+def zero_rule_algorithm_classification(train, test):
+    output_values = [row[-1] for row in train]
+    prediction = max(set(output_values), key=output_values.count)
+    predicted = [prediction for i in range(len(test))]
+    return predicted
+
+seed(1)
+filename = 'PimaData.csv'
+dataset = load_csv(filename)
+for i in range(len(dataset[0])):
+    str_column_to_float(dataset, i)
+split = 0.6
+accuracy = evaluate_algorithm(dataset, zero_rule_algorithm_classification, split)
+print('Train Test Split')
+print('Accuracy: %.3f%%' % (accuracy))
 
 # Harness for Cross Validation Evaluation
 
@@ -114,7 +115,6 @@ def accuracy_metric(actual, predicted):
 
 def evaluate_algorithm(dataset, algorithm, n_folds, *args):
     folds = cross_validation_split(dataset, n_folds)
-    print(len(folds))
     scores = list()
     for fold in folds:
         train_set = list(folds)
@@ -146,5 +146,6 @@ for i in range(len(dataset[0])):
 # evaluate algorithm
 n_folds = 5
 scores = evaluate_algorithm(dataset, zero_rule_algorithm_classification, n_folds)
+print('Cross Validation Split')
 print('Scores: %s' % scores)
 print('Mean Accuracy: %.3f%%' % (sum(scores)/len(scores)))
