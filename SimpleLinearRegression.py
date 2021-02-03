2937b2d01491ab32b2385495278d149debffe9cf
Inlic <bwiedeback@gmail.com>
2021-02-03 11:40:50 -0700

    testing simple linear vs zero rule prediction

--- SimpleLinearRegression.py
+++ SimpleLinearRegression.py
@@ -1,6 +1,33 @@
 # simple linear regression in python
 
 from math import sqrt
+from random import seed
+from random import randrange
+from csv import reader
+
+def load_csv(filename):
+    dataset = list()
+    with open(filename, 'r') as file:
+        csv_reader = reader(file)
+        for row in csv_reader:
+            if not row:
+                continue
+            dataset.append(row)
+    return dataset
+
+def string_to_float(dataset, column):
+    for row in dataset:
+        row[column] = float(row[column].strip())
+
+def train_test_split(dataset, split):
+    train = list()
+    train_size = split * len(dataset)
+    dataset_copy = list(dataset)
+    while len(train) < train_size:
+        index = randrange(len(dataset_copy))
+        train.append(dataset_copy.pop(index))
+    return train, dataset_copy
+
 
 def mean(values):
     return sum(values) / float(len(values))
@@ -66,3 +93,43 @@ b0, b1 = coefficients(dataset)
 print('Coefficients: B=%.3f, B=%f' % (b0,b1))
 rmse = evaluate_algorithm(dataset, simple_linear_regression)
 print('RMSE: %.3f' % (rmse))
+
+
+def evaluate_algorithm(dataset, algorithm, split, *args):
+    train, test = train_test_split(dataset, split)
+    test_set = list()
+    for row in test:
+        row_copy = list(row)
+        row_copy[-1] = None
+        test_set.append(row_copy)
+    predicted = algorithm(train, test_set, *args)
+    actual = [row[-1] for row in test]
+    rmse = rmse_metric(actual, predicted)
+    return rmse
+
+def zero_rule_algorithm_classification(train, test):
+    output_values = [row[-1] for row in train]
+    prediction = max(set(output_values), key=output_values.count)
+    predicted = [prediction for i in range(len(test))]
+    return predicted
+
+seed(1)
+filename = 'swedeInsurance.csv'
+dataset = load_csv(filename)
+for i in range(len(dataset[0])):
+    string_to_float(dataset, i)
+split = 0.6
+print('Baseline')
+baseline = evaluate_algorithm(dataset, zero_rule_algorithm_classification, split)
+print('Zero Rule RMSE: %.3f' %(baseline))
+print('Simple Linear Regression')
+seed(1)
+filename = 'swedeInsurance.csv'
+dataset = load_csv(filename)
+for i in range(len(dataset[0])):
+    string_to_float(dataset, i)
+split = 0.6
+rmse = evaluate_algorithm(dataset, simple_linear_regression, split)
+print('RMSE: %.3f' % (rmse))
+
+
