bf2974abfe6793be9129aeb0336ee31dc7246200
Inlic <bwiedeback@gmail.com>
2021-02-05 14:25:16 -0700

    logistic regression experimentation

--- Logistic Regression.py
+++ Logistic Regression.py
@@ -50,23 +50,41 @@ def accuracy_metric(actual, predicted):
             correct += 1
     return correct / float(len(actual)) * 100.0
 
+def evaluate_algorithm(dataset, algorithm, n_folds, *args):
+    folds = cross_validation_split(dataset, n_folds)
+    scores = list()
+    for fold in folds:
+        train_set = list(folds)
+        train_set.remove(fold)
+        train_set = sum(train_set, [])
+        test_set = list()
+        for row in fold:
+            row_copy = list(row)
+            test_set.append(row_copy)
+            row_copy[-1] = None
+        predicted = algorithm(train_set, test_set, *args)
+        actual = [row[-1] for row in fold]
+        accuracy = accuracy_metric(actual, predicted)
+        scores.append(accuracy)
+    return scores
+
 def predict(row, coefficients):
     yhat = coefficients[0]
     for i in range(len(row)-1):
         yhat += coefficients[i+1]*row[i]
     return 1.0 / (1.0 + exp(-yhat))
 
-dataset = [[2.7810836,2.550537003,0],
-[1.465489372,2.362125076,0],
-[3.396561688,4.400293529,0],
-[1.38807019,1.850220317,0],
-[3.06407232,3.005305973,0],
-[7.627531214,2.759262235,1],
-[5.332441248,2.088626775,1],
-[6.922596716,1.77106367,1],
-[8.675418651,-0.242068655,1],
-[7.673756466,3.508563011,1]]
-coef = [-0.406605464, 0.852573316, -1.104746259]
+# dataset = [[2.7810836,2.550537003,0],
+# [1.465489372,2.362125076,0],
+# [3.396561688,4.400293529,0],
+# [1.38807019,1.850220317,0],
+# [3.06407232,3.005305973,0],
+# [7.627531214,2.759262235,1],
+# [5.332441248,2.088626775,1],
+# [6.922596716,1.77106367,1],
+# [8.675418651,-0.242068655,1],
+# [7.673756466,3.508563011,1]]
+# coef = [-0.406605464, 0.852573316, -1.104746259]
 # for row in dataset:
 #     yhat = predict(row, coef)
 #     print("Expected=%.3f, Predicted=%.3f [%d]" % (row[-1], yhat, round(yhat)))
@@ -82,10 +100,32 @@ def coefficients_sgd(train, l_rate, n_epoch):
             coef[0] = coef[0] + l_rate * error * yhat * (1.0 - yhat)
             for i in range(len(row)-1):
                 coef[i + 1] = coef[i + 1] + l_rate * error * yhat * (1.0 - yhat) * row[i]
-        print('>epoch=%d, lrate=%.3f, error=%.3f' % (epoch, l_rate, sum_error))
     return coef
 
-l_rate = 0.3
-n_epoch = 100
-coef = coefficients_sgd(dataset, l_rate, n_epoch)
-print(coef)
+def logistic_regression(train, test, l_rate, n_epoch):
+    predictions = list()
+    coef = coefficients_sgd(train, l_rate, n_epoch)
+    for row in test:
+        yhat = predict(row, coef)
+        yhat = round(yhat)
+        predictions.append(yhat)
+    return(predictions)
+
+# l_rate = 0.3
+# n_epoch = 100
+# coef = coefficients_sgd(dataset, l_rate, n_epoch)
+# print(coef)
+
+seed(1)
+filename = 'PimaData.csv'
+dataset = load_csv(filename)
+for i in range(len(dataset[0])):
+    str_column_to_float(dataset, i)
+minmax = dataset_minmax(dataset)
+normalize_dataset(dataset,minmax)
+n_folds = 5
+l_rate = 0.21
+n_epoch = 250
+scores = evaluate_algorithm(dataset,logistic_regression, n_folds, l_rate, n_epoch)
+print('Score: %s' % scores)
+print('Mean Accuracy: %.3f%%' % (sum(scores)/float(len(scores))))
